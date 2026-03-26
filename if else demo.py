5730c53caaaa466ea01585a565cb5a76eeea7ca4
Sahil Kumar <54834362+Sahil4UI@users.noreply.github.com>
2026-03-26 12:42:41 +0530

    Add files via upload

---
+++ if else demo.py
@@ -0,0 +1,6 @@
+#conditional statements
+x = 5
+if x%2==0:
+    print("Even")
+else:
+    print("Odd")
---
+++ operator.py
@@ -0,0 +1,65 @@
+Python 3.14.2 (v3.14.2:df793163d58, Dec  5 2025, 12:18:06) [Clang 16.0.0 (clang-1600.0.26.6)] on darwin
+Enter "help" below or click "Help" above for more information.
+>>> #operator
+>>> #arithmetic operator
+>>> x = 12
+>>> y = 5
+>>> x+y
+17
+>>> x-y
+7
+>>> x/y
+2.4
+>>> x*y
+60
+>>> x/y
+2.4
+>>> x//y
+2
+>>> #floor div
+>>> 2**4
+16
+>>> 
+>>> #assignment operator
+>>> x = 5
+>>> x += 5
+>>> x
+10
+>>> x -= 5
+>>> x
+5
+x *=5
+x
+25
+x/=5
+x
+5.0
+
+#Relational Operator
+2>5
+False
+2<=2#less than or equals
+True
+2==2
+True
+2!=6
+True
+
+#logical operator
+#arithmetic
+22%7#modulus
+1
+
+===================== RESTART: /Users/sahil/Desktop/if else demo.py ====================
+Odd
+
+#walrus operator
+x = 5
+x = 10
+print(x)
+10
+
+print(x:=20)
+20
+x
+20
